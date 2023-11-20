cff6777e5483ab3f2aea5d9f0aa2bbcbb7dd4d35
boson <boson13579@gmail.com>
2023-11-20 23:35:43 +0800

    update

---
+++ test.ps1
@@ -0,0 +1,5 @@
+Start-Process -FilePath "python" -ArgumentList "server.py" -NoNewWindow
+
+Start-Process -FilePath "python" -ArgumentList "client.py" -NoNewWindow
+Start-Process -FilePath "python" -ArgumentList "client.py" -NoNewWindow
+Start-Process -FilePath "python" -ArgumentList "client.py" -NoNewWindow
